3d62681b8800932e1949f7ced2f2e3be2ec04b0d
JamieLHowell <jamielutherhowell@gmail.com>
2025-02-06 19:13:54 +0000

    Lighting Fix

--- Assets/Scripts/ShopScript.cs
+++ Assets/Scripts/ShopScript.cs
@@ -5,22 +5,37 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ShopScript : MonoBehaviour
-{      
+{
+    public int timesBoostPurchased = 0;
+    public int maxPurchases = 4;
+
     public int boostPurchased;
+    public int boostPurchasedDisplay;
     public TextMeshProUGUI boostPurchasedText;
 
     private void Update() {
-        boostPurchasedText.text = boostPurchased.ToString();
+        boostPurchasedDisplay = maxPurchases - boostPurchased;
+        boostPurchasedText.text = boostPurchasedDisplay.ToString();
     }
 
     public void BoostPurchased() 
     {
         if(GameObject.Find("GameManager").GetComponent<GameManager>().collectables >= 1) 
         {
-            GameObject.Find("GameManager").GetComponent<GameManager>().SubtractCoin();
-            GameObject.Find("Car").GetComponent<CarController>().SpeedBoostPurchased();
+            if(timesBoostPurchased < maxPurchases)
+            {
+                GameObject.Find("GameManager").GetComponent<GameManager>().SubtractCoin();
+                GameObject.Find("Car").GetComponent<CarController>().SpeedBoostPurchased();
 
-            boostPurchased++;
+                timesBoostPurchased++;
+                boostPurchased++;
+            }
+            else
+            {
+                Debug.Log("Over the max purchase limit");
+            }
+            
+            
 
         }
         else{
